b97e513de9bb1d05f4c4797487ef574642a476ca
drdonv <50760474+drdonv@users.noreply.github.com>
2020-12-29 09:13:34 -0800

    Reverted to old code, fix didn't work.

--- background.js
+++ background.js
@@ -1,19 +1,18 @@
-
 var date = new Date()
 var timeHour = date.getHours()
 var extensionIsOn = true
 console.log("works")
 
 function blockWebsites() {
+	if (timeHour >= 9 && timeHour <= 17 && extensionIsOn){
 		chrome.webRequest.onBeforeRequest.addListener(
-			if (timeHour >= 9 && timeHour <= 17 && extensionIsOn){
 			function(details) {return {cancel: true}},
 		
 			{urls: ["*://*.zedo.com/*", "*://*.doubleclick.net/*", "https://*.sonyied.queue-it.net/*", "https://*.direct-queue.playstation.com/*", "https://*.discord.com/*", "https://*.twitter.com/*", "https://*.youtube.com/*", "https://*.reddit.com/*", "https://*.instagram.com/*"]}, //"*://*www.youtube.com/api/stats/ads/*"]},
 	
 			["blocking"]
-		}
 		)
+	}
 }
 blockWebsites()
 // document.getElementById('press').addEventListener('click', blockWebsites());
